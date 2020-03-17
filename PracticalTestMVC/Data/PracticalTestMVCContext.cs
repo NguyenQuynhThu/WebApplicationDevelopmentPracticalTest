8f34c45d11244387133557fec07f62ffd15eb2b9
Nguyen Quynh Thu <letter.japan@gmail.com>
2020-03-17 20:32:50 +0700

    final

---
+++ PracticalTestMVC/Data/PracticalTestMVCContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PracticalTestMVC.Data
+{
+    public class PracticalTestMVCContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public PracticalTestMVCContext() : base("name=PracticalTestMVCContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<PracticalTestMVC.Models.Employee> Employees { get; set; }
+    }
+}
---
+++ PracticalTestMVC/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticalTestMVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string EmployeeID { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public double Slary { get; set; }
+    }
+}
